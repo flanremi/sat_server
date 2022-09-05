edb2049e3fdd83ba03baa56620aaacacc902c416
py_ubuntu <againxiansen@gmail.com>
2022-09-04 23:04:52 -0700

    95 1404

--- server/app.py
+++ server/app.py
@@ -4,12 +4,15 @@ import time
 from abc import ABC
 
 import requests as requests
+from threading import Lock
 from flask import Flask, request
 from concurrent.futures import ThreadPoolExecutor
 
 from alter.process import TimeCore, CoreInter
 import k8s_tools
 from time_vary_1 import Time_vary_1
+
+lock = Lock()
 
 
 class Listener(CoreInter, ABC):
@@ -43,6 +46,11 @@ core.run()
 # core.addRequest(1, "name1.txt")
 # while True:
 #     pass
+k8s_tools.deploySys()
+import player
+
+iplayer = player.Player()
+
 app = Flask(__name__)
 
 
@@ -62,7 +70,9 @@ def cdn():
         core.addRequest(client_num, name)
         file = core.getfile(client_num, name)
         if not file or file.get("is_cache") == 0:
-            url = k8s_tools.pushStream(k8s_tools.sname2ip("master"), k8s_tools.sname2ip(r.get("sname")), name, str(i))
+            url = k8s_tools.pushStream(k8s_tools.sname2ip("k8s-master"), k8s_tools.sname2ip(r.get("sname")), name,
+                                       str(i))
+            time.sleep(random.randint(2500, 5000) / 1000)
         else:
             url = k8s_tools.pushStream(k8s_tools.sname2ip(r.get("sname")), k8s_tools.sname2ip(r.get("sname")), name,
                                        str(i))
@@ -70,14 +80,65 @@ def cdn():
     return json.dumps(dis_result)
 
 
-# for i in range(100):
-#     t.addRequest(random.randint(0, 119),"aaa" + str(random.randint(0, 20)), 10)
-# t.refresh()
-# for i in range(5000):
-#     t.addRequest(random.randint(0, 119),"aaa" + str(random.randint(0, 20)), 10)
-# t.refresh()
-# while True:
-#     pass
+@app.route("/time_v", methods=['POST'])
+def time_v():
+    lat = request.form.get("lat")
+    lng = request.form.get("lng")
+    dur = request.form.get("dur")
+    t = Time_vary_1(lat, lng, dur).result()
+    # dis_result = []
+    # # test
+    # i = 0
+    # for r in t:
+    #     if i % 2 == 0:
+    return json.dumps(t)
+
+
+@app.route("/get_cache", methods=['POST'])
+def get_cache():
+    from server.pycode.leslie_sysinfo_code import get_all_node_name_ip
+    node = get_all_node_name_ip()
+    result = []
+
+    def get_client_cache(n: str, url: str):
+        global lock
+        res = requests.post("http://" + url + ":4995/query")
+        lock.acquire()
+        result.append({"name": n, "cache": res.text})
+        lock.release()
+
+    for name, ip in node:
+        theard_pool.submit(get_client_cache, name, ip)
+
+    while True:
+        time.sleep(0.5)
+        if len(result) == len(node):
+            break
+    return json.dumps(result)
+
+
+@app.route("/remove_sys", methods=['POST'])
+def remove_sys():
+    return k8s_tools.deleteSysDeploy()
+
+
+@app.route("/remove_video", methods=['POST'])
+def remove_video():
+    return k8s_tools.deleteVideoDeploy()
+
+
+@app.route("/play_video", methods=['POST'])
+def play_video():
+    url = request.form.get("url")
+    iplayer.play(url)
+    return "OK"
+
+
+@app.route("/stop_video", methods=['POST'])
+def stop_video():
+    iplayer.stop()
+    # iplayer.release()
+    return "OK"
 
 
 if __name__ == '__main__':
--- server/k8s_tools.py
+++ server/k8s_tools.py
@@ -10,8 +10,8 @@ from server.pycode.Node import *
 def sname2ip(sname):
     nodes = get_all_node_name_ip()
     # todo 映射节点名和卫星名
-    if sname == "master":
-        return Node({"Name":nodes[0][0], "Ip": nodes[0][1]})
+    if sname == "k8s-master":
+        return Node({"Name": nodes[0][0], "Ip": nodes[0][1]})
     else:
         return Node({"Name": nodes[1][0], "Ip": nodes[1][1]})
     # return Node({"Name": "ubuntu", "Ip": nodes[0][1]})
@@ -23,15 +23,17 @@ def pushStream(start: Node, target: Node, name, url_suf):
 
         yaml_file['spec']['template']['spec']['nodeName'] = str(start.Name)
         yaml_file['spec']['template']['spec']['volumes'][0]['hostPath']['path'] = "/home/dxh/cdn/"
+        # yaml_file['spec']['template']['spec']['volumes'][0]['hostPath']['path'] = "/home/flan/tmp/fs_tmp/"
 
         yaml_file['spec']['template']['spec']['containers'][0]['args'][2] = "/usr/app/tmp/" + name
+        # yaml_file['spec']['template']['spec']['containers'][0]['args'][2] = "/home/" + name
         yaml_file['spec']['template']['spec']['containers'][0]['args'][8] = "rtmp://" + target.Ip + "/live/" + name + \
                                                                             url_suf
 
-        changed_yaml_name = start.Name + get_r_code() + "-" + "ffmpeg_video.yaml"
+        changed_yaml_name = start.Name + get_r_code() + "-" + "ffmpeg_video"
         changed_yaml_name = changed_yaml_name.replace("_", "-")
         yaml_file['metadata']['name'] = changed_yaml_name
-        changed_yaml_name = "yaml_file/tmp/" + start.Name + get_r_code() + "_" + "ffmpeg_video.yaml"
+        changed_yaml_name = "yaml_file/tmp/yaml/" + start.Name + get_r_code() + "_" + "ffmpeg_video.yaml"
         changed_yaml_name = changed_yaml_name.replace("-", "_")
         with open(changed_yaml_name, 'w+') as out:
             yaml.dump(yaml_file, out)
@@ -41,4 +43,43 @@ def pushStream(start: Node, target: Node, name, url_suf):
         print(kube_apply_info)
         return "rtmp://" + target.Ip + "/live/" + name + url_suf
 
+
+def deploySys():
+    node = get_all_node_name_ip()
+    for name, ip in node:
+        with open("yaml_file/srs_run.yaml", 'r') as f:
+            yaml_file = yaml.load(f, Loader=yaml.FullLoader)
+            yaml_file['metadata']['labels']['app'] = "srs-" + name
+            yaml_file['metadata']['name'] = "srs-" + name + "-deployment"
+            yaml_file['spec']['selector']['matchLabels']['app'] = "srs-" + name
+            yaml_file['spec']['template']['metadata']['labels']['app'] = "srs-" + name
+            yaml_file['spec']['template']['spec']['nodeName'] = name
+            yaml_file['spec']['template']['spec']['containers'][0]['name'] = "srs-" + name
+            yaml_file_name = "yaml_file/tmp/sys/srs_run_" + name + ".yaml"
+            with open(yaml_file_name, "w") as out:
+                yaml.dump(yaml_file, out)
+            cmd = "kubectl apply -f " + yaml_file_name
+            subprocess.check_output(cmd, shell=True).decode('utf-8')
+
+
+def deleteSysDeploy():
+    infos = []
+    for root, dic, files in os.walk("yaml_file/tmp/sys/"):
+        for file in files:
+            cmd = "kubectl delete -f " + root + file
+            kube_apply_info = subprocess.check_output(cmd, shell=True).decode('utf-8')
+            infos.append(kube_apply_info)
+            os.remove(root + file)
+    return infos
+
+
+def deleteVideoDeploy():
+    infos = []
+    for root, dic, files in os.walk("yaml_file/tmp/yaml/"):
+        for file in files:
+            cmd = "kubectl delete -f " + root + file
+            kube_apply_info = subprocess.check_output(cmd, shell=True).decode('utf-8')
+            infos.append(kube_apply_info)
+            os.remove(root + file)
+    return infos
 # pushStream(sname2ip(""), sname2ip(""), "cheat.mp4", "11919")
